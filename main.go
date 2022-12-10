097452126e491b737398fde4a59d1d22c38718b5
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-12-10 21:24:28 +0900

    Primary - argument 스프레드 연산자(?)

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-// ,를 이용해서 여러 개를 반환할 수 있다!
-// Go에는 여러 멋진 패키지가 존재한다
-func lenAdnUpper(name string) (int, string) {
-	return len(name), strings.ToUpper((name))
+// 스프레드연산자..? 를 이용하면 여러 개를 받을 수 있다!
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	// 이렇게 불러올 수 있다!! _(underBar)를 사용하면 무시할 수 있다
-	totalLeng, upperName := lenAdnUpper("young")
-	fmt.Println(totalLeng, upperName)
+	repeatMe("abc", "young", "nico", "dal", "jung")
 }
