3b13cf290ae84bf319257539f2e8668cedb7581e
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-12-10 21:11:55 +0900

    Primary - 다중 반환 함수

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// ,를 이용해서 여러 개를 반환할 수 있다!
+// Go에는 여러 멋진 패키지가 존재한다
+func lenAdnUpper(name string) (int, string) {
+	return len(name), strings.ToUpper((name))
+}
 
 func main() {
-	// 상수 선언 법
-	const NAME string = "youngHun"
-	// 변수 선언 법
-	var name string = "young"
-	// 위와 같음 - 타입 추론을 해준다!!
-	name2 := "young" // string
-
-	fmt.Println("Hello World!" + name + name2)
+	// 이렇게 불러올 수 있다!! _(underBar)를 사용하면 무시할 수 있다
+	totalLeng, upperName := lenAdnUpper("young")
+	fmt.Println(totalLeng, upperName)
 }
