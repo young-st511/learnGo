dd69615a51422f4477d2075f0fdc80f7dd621cee
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-12-10 21:33:01 +0900

    Primary - 반복문!

--- main.go
+++ main.go
@@ -4,11 +4,28 @@ import (
 	"fmt"
 )
 
-// 스프레드연산자..? 를 이용하면 여러 개를 받을 수 있다!
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func superAdd(numbers ...int) int {
+	// for-in, for-of랑 비슷하다!
+	// range는 index와 변환되는 값을 반환한다
+	for index, number := range numbers {
+		fmt.Println(index, number)
+	}
+
+	total := 0
+
+	// 언더바 사용하면 컴파일러가 무시한다
+	for _, number := range numbers {
+		total += number
+	}
+
+	// 물론 전통적인 for문도 가능!!
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+
+	return total
 }
 
 func main() {
-	repeatMe("abc", "young", "nico", "dal", "jung")
+	superAdd(11, 22, 33, 44, 55, 66, 77, 88)
 }
