90e6a35bfd3400465f8a59be1fde8c25c25cd367
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-12-10 20:55:04 +0900

    Primary - Export

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-func superAdd(numbers ...int) int {
+import "fmt"
 
-}
-
+// PascalCase로 쓰여진 변수나 함수는 export 된다!
 func main() {
+	// ❌ error!
+	fmt.println("Hellow World!")
 
+	// ✅ OK!
+	fmt.Println("Hellow World!")
 }
