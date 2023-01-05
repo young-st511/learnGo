217910e3f222ac91ef5839675caa2dc57ef8b696
young-st511 <asdk511@gmail.com>
2023-01-05 14:33:12 +0900

    Primary - Pointers

--- README.md
+++ README.md
@@ -166,3 +166,24 @@ func main() {
 	fmt.Println(canIDrink(18))
 }
 ```
+
+## Pointer
+
+```go
+func main() {
+	a := 2
+	b := a
+	a = 10 // a = 10, b = 2
+	fmt.Println(a, b)
+
+	aa := 2
+	bb := &aa
+	//# & -> 메모리 주소를 볼 수 있다
+	//# * -> 해당 주소에 들어가 값을 볼 수 있다
+	aa = 6
+	fmt.Println(aa, *bb) // aa = 6, *bb = 6
+	*bb = 0
+	fmt.Println(aa, *bb) // aa = 0, *bb = 0
+
+}
+```
--- main.go
+++ main.go
@@ -4,30 +4,19 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	// switch문도 variable expression 사용 가능!
-	switch koreanAge := age + 2; koreanAge {
-	case 18:
-		return true
-	case 10:
-		return false
-	case 50:
-		return true
-	}
-	return false
-}
-
-func canIDrink2(age int) bool {
-	// switch문에 조건문도 사용 가능
-	switch koreanAge := age + 2; {
-	case koreanAge < 18:
-		return false
-	case koreanAge > 70:
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := a
+	a = 10 // a = 10, b = 2
+	fmt.Println(a, b)
+
+	aa := 2
+	bb := &aa
+	//# & -> 메모리 주소를 볼 수 있다
+	//# * -> 해당 주소에 들어가 값을 볼 수 있다
+	aa = 6
+	fmt.Println(aa, *bb) // aa = 6, *bb = 6
+	*bb = 0
+	fmt.Println(aa, *bb) // aa = 0, *bb = 0
+
 }
